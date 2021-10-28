d82df2295434bc5badb045e1950da45eb0f4ef89
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-10-28 14:16:21 -0400

    added resume content

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,9 +3,91 @@ import React from 'react';
 function Resume() {
 
     return(
-        <header>
-            This is my Resume.
-        </header>
+    <section id="Experience">
+        <div class="roles">
+        <h2 class="section-title">Experience</h2>
+        <div class="role">
+            <h3 class="company">
+                Suncor Energy Inc.
+            </h3>
+            <h4 class="position">
+                Category Specialist, Car Wash – Category Management
+            </h4>
+            <ul>
+                <li>
+                    Delivered two annual business plans for the car wash category, using strategic planning and analytical skills to develop
+                    key growth initiatives, P&L forecasting, budget allocation, and marketing campaigns, in order to achieve record
+                    financial results of $98M in sales and $54M in margin.
+                </li>
+                <li>
+                    Oversaw multiple capital investment initiatives using project management and stakeholder management skills to
+                    deliver asset upgrades and marketing initiatives to provide a best in class value proposition and customer experience.
+                </li>
+                <li>
+                    Responsible for all marketing programs and campaigns, using communication and collaboration skills to bring them to
+                    market and ensure successful execution by operations, resulting in 8% YoY unit growth and 50% market share.
+                </li>
+                <li>
+                    Led a 5 year strategic business review, using analytical and collaboration skills, which lead to the transformation of
+                    campaign strategy, product prioritization, and channel development.
+                </li>
+            </ul>
+        </div>
+        <div class="role">
+                <h3 class="company">Suncor Energy Inc.</h3>
+                <h4 class="position">Advisor, Card Production – Transaction Management</h4>
+                <ul>
+                    <li>
+                        Brought virtual cards to market, using project management and collaboration skills to work with the development
+                        team and sales group to deliver necessary system enhancements and process frameworks needed to launch.
+                    </li>
+                    <li>
+                        As RFP project lead, collaborated with Supply Chain to develop the scope of work and award contracts to 2 vendors,
+                        achieving $1.2M in cost savings over two 3 year contracts while also securing improved terms on quality assurance and
+                        service level requirements.
+                    </li>
+                    <li>
+                        Used analytics to develop new inventory management score-carding, which overhauled the Card Production supply
+                        and distribution chains which enabled the reduction of inventory by 50%, and realized over $250K in cost savings in
+                        the first year.
+                    </li>
+                </ul>
+        </div>
+        <div class="role">
+                <h3 class="company">Suncor Energy Inc.</h3>
+                <h4 class="position">Optimization Analyst – Refining and Logistics</h4>
+                <ul>
+                    <li>
+                        Conducted market research and analysis using Argus, in order to prepare weekly reports for Executives with insights
+                        on the business environment in the O&G Industry, which was used to support decision making.
+                    </li>
+                    <li>
+                        Through analysis and collaboration with stakeholders, assisted in the preparation of short term refinery production
+                        estimates and pricing forecasts to optimize refinery capacity utilization and maximize profitability of refined products.
+                    </li>
+                    <li>
+                        Used organization skills and attention to detail to reliably deliver business performance and pricing reports to
+                        stakeholders, in order to support trading activities and accounting processes. 
+                    </li>
+                </ul>
+        </div>
+        <div class="role">
+                <h3 class="company">Suncor Energy Inc.</h3>
+                <h4 class="position">Account Manager – Commercial Road Transport</h4>
+                <ul>
+                    <li>
+                        Managed a base client portfolio worth $40M, developing sales and relationship management skills to retain existing
+                        business while growing to new clients, resulting in a 10% increase in base accounts within my portfolio.
+                    </li>
+                    <li>
+                        Prepared pitch decks and RFP proposals for high value clients, using communication and presentation skills to win
+                        new business, including the closing of FedEx and City of Toronto accounts worth over 100ML in combined volume.
+                    </li>
+                </ul>
+        </div>
+        </div>
+    </section>
+
     )
 }
 
