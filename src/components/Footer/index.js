4fb2c8bbef373052e8b1963dd5bf78bd78067ca2
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-11-07 10:43:33 -0500

    footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,28 +3,30 @@ import React from 'react';
 function Footer() {
 
     return(
-        <footer className="row">
-            <div>
-                <h2 className="col mx-1" id="footer-name">
+        <footer className="container">
+            <div className="row footer-name">
+                <h2>
                     Made by: Jason Grossman
                 </h2>
                 <h3>
                     Powered with React
                 </h3>
-            <h3 className="col mx-2">
-                <span> 
-                    <a href="https://github.com/jasongrossman">
-                    Checkout my GitHub page here:
-                    </a>
-                </span>
-            </h3>
-            <h3 className="col mx-2">
-                <span> 
-                    <a href="https://www.linkedin.com/in/jason-grossman-1a082430/">
-                    Connect with me on LinkedIn here:
-                    </a>
-                </span>
-            </h3>
+            </div>
+            <div className="row">
+                <h3 className="col">
+                    <span> 
+                        <a href="https://github.com/jasongrossman">
+                        Github
+                        </a>
+                    </span>
+                </h3>
+                <h3 className="col">
+                    <span> 
+                        <a href="https://www.linkedin.com/in/jason-grossman-1a082430/">
+                        LinkedIn
+                        </a>
+                    </span>
+                </h3>
             </div>
         </footer>
     )
