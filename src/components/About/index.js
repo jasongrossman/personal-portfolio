d871c8c139281c0546fd7bd09671a07d6fed81b0
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-12-20 15:57:49 -0500

    about me media queries

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,45 +6,51 @@ function About() {
 
     return(
         <section className="about-body">
-        <div className="bio">
-            <img src={profilePic} alt="cover" className="cover-photo"/>
-            <div>
-            <h1 className="about-title">
-                 About Me
-            </h1>
+        <div className="bio row">
+            <img src={profilePic} alt="cover" className="cover-photo col-lg-4 col-md-12"/>
+            <div className="col-lg-7 col-md-12">
+                <h1 className="about-title">
+                    About Me
+                </h1>
                 <p className="about-text">
-                Full stack web developer with professional business experience in category management and analytics. 
-                Earned a certificate in full stack development from the University of Toronto School of Continuing Studies. 
-                Now proficient in Javascript, Node, React, MongoDB as well as responsive web design. 
-                With a history in project management and bringing products to market, I can now plan and ship development features as well. 
-                I’m passionate about solving problems, and using technology to create new solutions. 
-                I’m an entrepreneur at heart who loves to build and create, while using a data-driven, strategic approach. 
-                I am seeking an opportunity to learn and refine my development capabilities so that I can build better applications independently. 
-                As a strong communicator who can adapt to any team environment, I’m excited to leverage my experience to join a quality-driven team to build great products.
+                    Full stack web developer with professional business experience in category management and analytics. 
+                    Earned a certificate in full stack development from the University of Toronto School of Continuing Studies. 
+                    Now proficient in Javascript, Node, React, MongoDB as well as responsive web design. 
+                    With a history in project management and bringing products to market, I can now plan and ship development features as well. 
+                    I’m passionate about solving problems, and using technology to create new solutions. 
+                    I’m an entrepreneur at heart who loves to build and create, while using a data-driven, strategic approach. 
+                    I am seeking an opportunity to learn and refine my development capabilities so that I can build better applications independently. 
+                    As a strong communicator who can adapt to any team environment, I’m excited to leverage my experience to join a quality-driven team to build great products.
                 </p>
             </div>
         </div>
-        <div className="card-group">
-            <div className="card">
-                <img src={require(`../../assets/about/coding_card.png`).default} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                <h5 className="card-title">Full Stack Development</h5>
-                <p className="card-text">Capable of building responsive web applications, using MERN stack. Uses mobile-first development processes, with an eye for U/X. Knowledgeable about industry best practices for design solutions to optimize for user goal completion.</p>
+        <div className="card-group row">
+            <div className="card-container col-lg-4 col-md-12">
+                <div className="card col-md-12">
+                    <img src={require(`../../assets/about/coding_card.png`).default} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                    <h5 className="card-title">Full Stack Development</h5>
+                    <p className="card-text">Capable of building responsive web applications, using MERN stack. Uses mobile-first development processes, with an eye for U/X. Knowledgeable about industry best practices for design solutions to optimize for user goal completion.</p>
+                    </div>
                 </div>
             </div>
-            <div className="card">
+            <div className="card-container col-lg-4 col-md-12">
+            <div className="card  col-md-12">
             <img src={require(`../../assets/about/project_management_card.png`).default} className="card-img-top" alt="..."/>
                 <div className="card-body">
                 <h5 className="card-title">Project Management</h5>
                 <p className="card-text">Experience in Agile project management methodology, as well as Waterfall. Led multiple project teams with cross functional partners to ship features to market and grow KPIs. A natural leader who can organize large and small teams to get things done on time and in full.</p>
                 </div>
             </div>
-            <div className="card">
+            </div>
+            <div className="card-container col-lg-4 col-md-12">
+            <div className="card col-md-12">
             <img src={require(`../../assets/about/strategy_card.png`).default} className="card-img-top" alt="..."/>
                 <div className="card-body">
                 <h5 className="card-title">Strategy & Analytics</h5>
                 <p className="card-text">Knows how to create short term and long term strategies, leveraging data to inform decision making. Whether that be growth through operational efficiency or innovation, can map out strategies and execute them.</p>
                 </div>
+            </div>
             </div>
         </div>
     </section>
